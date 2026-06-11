909ed64f6b14bc81be68f0c366b920f54b30caec
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-06-11 12:29:24 +0530

    recommend.js altered

--- routes/recommend.js
+++ routes/recommend.js
@@ -4,12 +4,13 @@ import cars from  '../data/cars.json' with { type: 'json' };
 const router = express.Router();
 
 router.get('/recommend', (req, res) => {
-    const { fuelType, seatingCapacity, usage } = req.query;
+    const { fuelType, familySize, usageType, budget } = req.query;
 
     const recommendedCars = cars.filter(car => {
-        return (!fuelType || car.fuelType === fuelType) &&
-               (!seatingCapacity || car.seatingCapacity === Number(seatingCapacity)) &&
-               (!usage || car.usage.includes(usage));
+        return (!fuelType || car.fuelType.toLocaleLowerCase() === fuelType.toLocaleLowerCase()) &&
+               (!familySize || car.seatingCapacity >= Number(familySize)) &&
+               (!usageType || car.usage.includes(usageType.toLocaleLowerCase())) &&
+               (!budget || car.price <= Number(budget));
     });
 
     res.json(recommendedCars);
