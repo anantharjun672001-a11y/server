cefe630497af7912e4847ef9234203fb67ffdc27
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-06-11 14:54:45 +0530

    fix cors url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors({
     origin:[
-        "https://client-eight-mu-53.vercel.app/",
+        "https://client-eight-mu-53.vercel.app",
         "http://localhost:5173"
     ],
     methods: ["GET", "POST"],
